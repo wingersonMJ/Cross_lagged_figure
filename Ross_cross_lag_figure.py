a825007b52d1eda0dffbfd39d52b91eb16c6e650
Mathew <mat.wingerson@gmail.com>
2025-10-07 16:29:16 -0600

    add .py file to repo

---
+++ Ross_cross_lag_figure.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Load data
+df = pd.read_excel("C:\\Users\\wingersm\\OneDrive - The University of Colorado Denver\\Desktop\\Python Projects\\16.0 Ross_cross_lag\\Cross-lagged sleep_activity\\crosslag_sleep_pa.xlsx", sheet_name="Final_dat")
+
+# mvpa1_7
+# mvpa8_14
+# awake1_7
+# awake8_14
+
+fig, axes = plt.subplots(1, 2, figsize=(10, 7))
+
+sns.boxplot(data=df[['mvpa1_7', 'mvpa8_14']], ax=axes[0], color='lightgrey', showfliers=False)
+sns.stripplot(data=df[['mvpa1_7', 'mvpa8_14']], ax=axes[0], color="dimgrey", size=4, jitter=True)
+axes[0].set_xticklabels(["Early MVPA", "Later MVPA"], fontsize=12)
+axes[0].set_ylabel("Average daily MVPA (minutes)", fontsize=12)
+
+sns.boxplot(data=df[['awake1_7', 'awake8_14']], ax=axes[1], color='lightgrey', showfliers=False)
+sns.stripplot(data=df[['awake1_7', 'awake8_14']], ax=axes[1], color="dimgrey", size=4, jitter=True)
+axes[1].set_xticklabels(["Early time in bed awake", "Later time in bed awake"], fontsize=10)
+axes[1].set_ylabel("Average daily time in bed awake (minutes)", fontsize=12)
+
+axes[0].text(
+    -0.10, 1.05, "A.", 
+    transform=axes[0].transAxes, 
+    fontsize=16, fontweight="bold", va="top", ha="right"
+)
+
+axes[1].text(
+    -0.10, 1.05, "B.", 
+    transform=axes[1].transAxes,
+    fontsize=16, fontweight="bold", va="top", ha="right"
+)
+
+plt.tight_layout()
+plt.savefig("C:\\Users\\wingersm\\OneDrive - The University of Colorado Denver\\Desktop\\Python Projects\\16.0 Ross_cross_lag\\Figure_1.jpg", dpi=300, bbox_inches='tight')
+plt.show()
+
+# testing out branches in git within VSCode. Kinda neat...
